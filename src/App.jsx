5da30179f21a67012dae4c3c62f01be726ef0b3f
Victor <aue228lublupacanov@gmail.com>
2023-11-21 20:31:51 +0200

    application created

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h2>Customer App</h2>
+    </>
+  )
+}
+
+export default App
